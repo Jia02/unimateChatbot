327c91a4293cda5e4334ebdad0295650234af1c3
Jia02 <yujialim0628@gmail.com>
2023-11-15 23:56:52 +0800

    modifed code to remove GPT-2 model, edit the interface

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,61 +1,50 @@
-# https://unimate-chatbot.streamlit.app/
-# github repo: https://github.com/Jia02/unimateChatbot 
-# python -m streamlit run streamlit_app.py
-# pip install -r requirements.txt
-
 import streamlit as st
 import re
 import torch
 import nltk
 from streamlit_extras.colored_header import colored_header
-from streamlit_extras.add_vertical_space import add_vertical_space
 from nltk.tokenize import sent_tokenize
-from transformers import GPT2Tokenizer, GPT2LMHeadModel, AutoModelWithLMHead, AutoTokenizer
+from transformers import AutoModelWithLMHead, AutoTokenizer
 from transformers import pipeline
 import random
 
-
 #======================Streamlit Application =================================
-st.set_page_config(page_title="UniMate : Your SU Mate",page_icon="💬")
-st.title('🤖💬 UniMate Chatbot 💬')
+
+st.set_page_config(page_title="UniMate - Your University Mate",page_icon="💬")
+st.title('UniMate Chatbot 💬🤖')
 
 user_selected_model = st.selectbox(
     "Select a model",
-    ("DialoGPT-small", "gpt-2", "gpt-2-qa")
+    ("GPT-2", "DialoGPT")
 )
 
 colored_header(label='', description='', color_name='blue-100')
 
 # Configure the side bar
 with st.sidebar:
-    st.title('💬 UniMate : Your University Mate')
-    st.markdown('''
-    ## About
-    This chatbot application aims to provide academic advising to the Sunway University's community, particularly knwoledgeable about the information regarding Student Handbook. 
-                           
-    ## Specifications
-    This chatbot is built using:
-    - Fine-tuned [GPT-2 model](https://huggingface.co/gpt2)
-    - Fine-tuned [microsoft/DialoGPT-small](https://huggingface.co/microsoft/DialoGPT-small)
-    - Streamlit
-    
-    💡 Note: No API key required!
+    st.markdown('''                  
+        ## About
+        This UniMate chatbot application aims to provide academic advising to the community of Sunway University / College , particularly knwoledgeable about the information regarding Student Handbook. 
+                            
+        ## Specifications
+        This chatbot is built using:
+        - Fine-tuned [GPT-2 model](https://huggingface.co/gpt2)
+        - Fine-tuned [microsoft/DialoGPT-small](https://huggingface.co/microsoft/DialoGPT-small)
+        - Streamlit
+        
+        💡 Note: No API key required!
+        
+        ### Connect with me ヽ( ´ ∀ `)ノ 
     ''')
-    add_vertical_space(4)
-    st.write('Made by [LYJ](<https://github.com/Jia02>)')
+
+    #Configure the layout of the social media icons
+    column1, column2 = st.columns(2)
+    column1.markdown("[![LinkedIn](<https://cdn2.iconfinder.com/data/icons/social-media-2285/512/1_Linkedin_unofficial_colored_svg-48.png>)](<https://www.linkedin.com/in/yujia-lim-b85081213/>)")
+    column2.markdown("[![GitHub](<https://img.icons8.com/material-outlined/48/000000/github.png>)](<https://github.com/jia02>)")
 
 #========================== Configure the model =================================
-# Define remote directory to the Huggingface Space's GPT model
-gpt_model_path = "YJia/gpt2-test" 
-@st.cache_resource
-def load_gpt_model(path):
-    gpt_model = GPT2LMHeadModel.from_pretrained(path)
-    gpt_tokenizer = GPT2Tokenizer.from_pretrained(path)
-    return gpt_model, gpt_tokenizer 
 
-gpt_model, gpt_tokenizer = load_gpt_model(gpt_model_path)
-
-# Define remote directory to the Huggingface Space's dialo-GPT model
+# Define remote directory to the fine-tuned Huggingface Space's DialoGPT model
 dialogpt_model_path = "YJia/dialogpt-test"
 @st.cache_resource
 def load_dialogpt_model(path):
@@ -66,8 +55,8 @@ def load_dialogpt_model(path):
 
 dialogpt_model, dialogpt_tokenizer = load_dialogpt_model(dialogpt_model_path)
 
-# Define remote directory to the Huggingface Space's gptQA model
-gptQA_model_path = "YJia/gptQA-test"
+# Define remote directory to the fine-tuned Huggingface Space's GPT-2 model
+gpt2QA_model_path = "YJia/gptQA-test"
 @st.cache_resource
 def load_gpt2QA_model(path):
     gpt2QA_pipeline = pipeline(
@@ -79,50 +68,16 @@ def load_gpt2QA_model(path):
     )
     return gpt2QA_pipeline
 
-gpt2QA_question_answerer = load_gpt2QA_model(gptQA_model_path)
+gpt2QA_question_answerer = load_gpt2QA_model(gpt2QA_model_path)
 
 #======================Configure the generation of response =============================
-# Function for generating LLM response from gpt2 model
-def gpt2_generate_response(model, tokenizer, prompt, max_length):
-    input_ids = tokenizer.encode(prompt, return_tensors="pt")
 
-    # Create the attention mask and pad token id
-    attention_mask = torch.ones_like(input_ids)
-    pad_token_id = tokenizer.eos_token_id
-
-    # Configure the hyperparameters when generating response
-    output = model.generate(
-        input_ids,
-        max_length=max_length,
-        num_return_sequences=1,
-        attention_mask=attention_mask,
-        pad_token_id=pad_token_id,
-        do_sample=True,
-        temperature=0.3,
-        top_k=20,
-        top_p=0.2,
-        repetition_penalty=1.0
-    ) 
-
-    # Convert the output to a string
-    response = tokenizer.decode(output[0])
-
-    # Use regular expressions to extract text within double quotes
-    matches = re.findall(r'"text": "([^"]*)"', response)
-
-    if matches:
-        generated_text = matches[0]
-
-    else:
-        generated_text = response  # Use the full response if "text" field not found
-
-    return generated_text
-
-# Function for generating LLM response from dialogpt model
+# Function to extract complete sentence from the generated response
 def is_complete_sentence(sentence):
     # Check if a sentence ends with a period, question mark, or exclamation mark
     return sentence.endswith('.') or sentence.endswith('?') or sentence.endswith('!')
 
+# Function for generating LLM response from DialoGPT model
 def query(payload, maxLength, model, tokenizer): 
     bot_input_ids = tokenizer.encode(payload["inputs"]["text"] + tokenizer.eos_token, return_tensors='pt')
 
@@ -149,18 +104,23 @@ def query(payload, maxLength, model, tokenizer):
         
     return {"generated_text": extracted_response}
 
-# Function for generating LLM response from gpt2-QA model
+# Function for generating LLM response from GPT-2 model
 def gpt2QA_generate_response(prompt):
 
-    answers = []
+    if "~" not in prompt:
+        return "I don't understand what you are asking. Please ask a complete prompt with a question together with the attached context."
+
     split_prompt = prompt.split("~")
     question = split_prompt[0].strip()
     context = split_prompt[1].strip()
 
     result = gpt2QA_question_answerer(question=question, context=context)
-    
+
+    answers = []
     for example in result:
-        answers.append(example['answer'])
+        answer = example['answer'].strip()
+        if (answer not in ["i", ".", "Add/Drop period.", "the Subject Add/Drop period."]):
+            answers.append(answer)
 
     # Choose a random answer from the list
     selected_answer = random.choice(answers)
@@ -198,28 +158,23 @@ if st.session_state.messages[-1]["role"] != "assistant":
     with st.chat_message("assistant"):
         with st.spinner("Generating a response..."):
             try:
-                if user_selected_model == "gpt-2": 
-                    # Load the response generated by the fine-tuned gpt2 model
-                    gpt2_response = gpt2_generate_response(gpt_model, gpt_tokenizer, prompt, 200)
-                    st.write(gpt2_response) 
-                    response = gpt2_response
-                    
-                elif user_selected_model == "gpt-2-qa":
-                    # Load the response generated by the fine-tuned gpt2-QA model
-                    gpt2_response = gpt2QA_generate_response(prompt)
-                    st.write(gpt2_response) 
-                    response = gpt2_response
-                    
-                else:
+                if user_selected_model == "DialoGPT":
                     # Load the response generated by the fine-tuned dialogpt model
                     dialogpt_response = query( {"inputs": {"text": prompt}}, 150, dialogpt_model, dialogpt_tokenizer)
                     st.write(dialogpt_response["generated_text"]) 
                     response = dialogpt_response["generated_text"]
 
+                else: #user_selected_model == "GPT-2"
+                    # Load the response generated by the fine-tuned GPT-2 model
+                    gpt2_response = gpt2QA_generate_response(prompt)
+                    st.write(gpt2_response) 
+                    response = gpt2_response
+
             except Exception as e:
                 st.error(f"An error occurred during response generation: {str(e)}")
                 # Update the chat history with the error message
                 st.session_state.messages.append({"role": "assistant", "content": f"An error occurred: {str(e)}"})
+                response = "I don't understand what you are asking. Please ask a complete question."
 
     # Now you can use the response variable in the rest of your code
     message = {"role": "assistant", "content": response}
